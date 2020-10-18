15e9240182c160b8a463dec7892eeed752a890b4
Thomas <thomas.cancino2014@gmail.com>
2020-10-17 22:24:50 -0300

    registro de usuario, con devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:first_name, :last_name, :email, :password, :password_confirmation, :rut, :dv )
+    end
+  end
 end
