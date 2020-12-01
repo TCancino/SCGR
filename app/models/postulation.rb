ea4ca067ab83a35c90e7db77b0d11396691dc902
RicardoAndres30 <ricaardo534@hotmail.com>
2020-11-30 22:19:54 -0300

    BDD actualizada: relaciones completas

--- app/models/postulation.rb
+++ app/models/postulation.rb
@@ -2,4 +2,5 @@ class Postulation < ApplicationRecord
     has_and_belongs_to_many :documents
     belongs_to :post
     belongs_to :postulation_state
+    belongs_to :user
 end
---
+++ db/migrate/20201201004344_add_user_type_to_user.rb
@@ -0,0 +1,5 @@
+class AddUserTypeToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :users, :user_type, null: false, foreign_key: true
+  end
+end
