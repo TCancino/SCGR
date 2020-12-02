a4b83335b187d5d9988bc61b128bc8720f063b39
Aangust <agustin.carmona@mail.udp.cl>
2020-12-01 21:29:14 -0300

    Comentar significado de los numeros en user.role

--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_230801) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "state"
-    t.integer "role", default: 0
+    t.integer "role", default: 0 #0-> Admin, 1-> Consultor, 2->Cliente, 3->Postulante
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
