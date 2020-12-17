a7e418fcc010d48f8d5868f6502bc336cdd2a003
RicardoAndres30 <ricaardo534@hotmail.com>
2020-12-17 00:43:46 -0300

    Trabajando en el delete de documentos

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,13 +4,15 @@ class DocumentsController < ApplicationController
   # GET /documents
   # GET /documents.json
   def index
-    @documents = Document.all
+    @documents = User.all
   end
 
   # GET /documents/1
   # GET /documents/1.json
   def show
-    @document = Document.find(params[:id])
+    #debugger
+    #@document = Document.find(params[:id])
+    render 'documents/show'
   end
 
   # GET /documents/new
@@ -25,6 +27,7 @@ class DocumentsController < ApplicationController
   # POST /documents
   # POST /documents.json
   def create
+    #debugger
     doc_name = document_params[:name]
     doc_file = document_params[:link]
     @document = Document.new(name: doc_name, link: doc_file, user_id: current_user.id)
@@ -57,9 +60,10 @@ class DocumentsController < ApplicationController
   # DELETE /documents/1
   # DELETE /documents/1.json
   def destroy
-    @document.destroy
+    #debugger
+    @document.purge_later
     respond_to do |format|
-      format.html { redirect_to documents_url, notice: 'Document was successfully destroyed.' }
+      format.html { redirect_to documents_path, notice: 'Documento eliminado exitosamente.' }
       format.json { head :no_content }
     end
   end
@@ -67,11 +71,12 @@ class DocumentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_document
-      @document = Document.find(params[:id])
+      debugger
+      @document = ActiveStorage::Blob.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def document_params
-      params.require(:document).permit(:name, :link)
+      params.require(:document).permit(:id)
     end
 end
