4049dcb74584010e6d8ac35f752c032a896d18a4
Aangust <agustin.carmona@mail.udp.cl>
2020-12-18 15:55:17 -0300

    Se puden editar algunos datos de los usuarios desde el index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,33 @@ class UsersController < ApplicationController
       @how_many = @how_many + 1
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :index, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def set_post
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :id,
+      :first_name,
+      :last_name,
+      :rut,
+      :dv,
+      :role
+    )
+  end
 end
