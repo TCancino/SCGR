e7817febe625f0ba1f88688df11acb362a8b8ce0
Thomas <thomas.cancino2014@gmail.com>
2020-12-22 02:16:42 -0300

    send state into mail

--- app/mailers/postulation_mailer.rb
+++ app/mailers/postulation_mailer.rb
@@ -1,8 +1,9 @@
 class PostulationMailer < ApplicationMailer
   default from: 'SCGR@mail.com'
 
-  def postulation_state_message(user_mail)
+  def postulation_state_message(user_mail, state)
     @name = user_mail.first_name || '-'
+    @state = state || '-'
     mail(
       to: user_mail.email,
       subject: 'Estado de su postulación'
